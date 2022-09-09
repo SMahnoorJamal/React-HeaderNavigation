968fd1f2c8bbae128a691365a4157b4d087762c2
Mahnoor Jamal <mahnoorjamal219@gmail.com>
2022-09-09 16:19:40 +0500

    HeaderNavigatin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './pages';
+import AnnualReport from './pages/annual';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let routes = useRoutes([                               //console shows how to implement routes
+    { path: '/', element: <Home /> },
+    { path: '/about', element: <About /> },
+    { path: '/annual', element: <AnnualReport /> },
+    { path: "/Blogs", element: <Blogs /> },
+    // ...
+  ]);
+  return routes;
 }
 
-export default App;
+const AppWrapper = () => {
+  return (
+    <Router>
+      <Navbar />
+      <App />
+    </Router>
+  );
+};
+
+export default AppWrapper;
